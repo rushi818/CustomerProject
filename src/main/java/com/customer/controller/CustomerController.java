e3c5da6babf00c6186b49dec94e6271797b437fd
rushi818 <rushikeshbhoi33@gmail.com>
2025-06-17 23:01:41 +0530

    changed spring boot version

--- src/main/java/com/customer/controller/CustomerController.java
+++ src/main/java/com/customer/controller/CustomerController.java
@@ -1,8 +1,10 @@
 package com.customer.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/customers")
 public class CustomerController {
 
 }
